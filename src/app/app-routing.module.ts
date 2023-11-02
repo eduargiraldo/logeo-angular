3348cc13d4ab9958b9025f965529eedd6b021ead
eduargiraldo <139786773+eduargiraldo@users.noreply.github.com>
2023-11-02 16:50:07 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistroComponent } from './registro/registro.component';
+
+const routes: Routes = [
+  {path:'', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'registro', component: RegistroComponent}
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  nombre:String='';
+  password:String='';
+
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void{
+
+
+  }
+  mostrarNombre(){
+    this.nombre=''
+
+  }
+
+ 
+  
+}
